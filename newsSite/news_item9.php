b2d316f6bc1786238cb5323eba4de87d5fbb34a8
Meritas <bubobuboster@gmail.com>
2013-07-17 14:09:32 +0300

    News loading from database
    Index now loads top 10 news from database. AJAX switching between news
    added as well.

---
+++ newsSite/news_item9.php
@@ -0,0 +1,18 @@
+<?php
+
+$mysqli = new mysqli("localhost",  "root", "", "newssite");
+
+if( $mysqli->connect_errno){
+    echo "Connection failed";
+    exit();
+}
+
+$query = $mysqli->query( "SELECT content FROM news_items
+ORDER BY id ASC");
+$result = $query->fetch_all();
+$query = $mysqli->query( "SELECT COUNT(id) FROM news_items");
+$number = $query->fetch_row();
+echo $result[$number[0]-9][0];
+
+$mysqli->close();
+?>
---
+++ newsSite/p-script.js
@@ -0,0 +1,200 @@
+$(document).ready(function(){
+	$(contentTextBox).load("news_item.php", function(responseText, statusText, xhr){
+
+	});
+});
+
+$(document).ready(function(){
+	$("#n1").click(function(event){
+		$("#contentTextBox").load("news_item.php", function(responseText, statusText, xhr){
+			/*if(statusText==="error"){
+				alert("Error loading content!");
+			}*/
+		});
+	});
+});
+
+$(document).ready(function(){
+	$("#n2").click(function(event){
+		$("#contentTextBox").load("news_item2.php", function(responseText, statusText, xhr){
+			/*if(statusText==="error"){
+				alert("Error loading content!");
+			}*/
+		});
+	});
+});
+
+$(document).ready(function(){
+	$("#n3").click(function(event){
+		$("#contentTextBox").load("news_item3.php", function(responseText, statusText, xhr){
+			/*if(statusText==="error"){
+				alert("Error loading content!");
+			}*/
+		});
+	});
+});
+
+$(document).ready(function(){
+	$("#n4").click(function(event){
+		$("#contentTextBox").load("news_item4.php", function(responseText, statusText, xhr){
+			/*if(statusText==="error"){
+				alert("Error loading content!");
+			}*/
+		});
+	});
+});
+
+$(document).ready(function(){
+	$("#n5").click(function(event){
+		$("#contentTextBox").load("news_item5.php", function(responseText, statusText, xhr){
+			/*if(statusText==="error"){
+				alert("Error loading content!");
+			}*/
+		});
+	});
+});
+
+$(document).ready(function(){
+	$("#n6").click(function(event){
+		$("#contentTextBox").load("news_item6.php", function(responseText, statusText, xhr){
+			/*if(statusText==="error"){
+				alert("Error loading content!");
+			}*/
+		});
+	});
+});
+
+$(document).ready(function(){
+	$("#n7").click(function(event){
+		$("#contentTextBox").load("news_item7.php", function(responseText, statusText, xhr){
+			/*if(statusText==="error"){
+				alert("Error loading content!");
+			}*/
+		});
+	});
+});
+
+$(document).ready(function(){
+	$("#n8").click(function(event){
+		$("#contentTextBox").load("news_item8.php", function(responseText, statusText, xhr){
+			/*if(statusText==="error"){
+				alert("Error loading content!");
+			}*/
+		});
+	});
+});
+
+$(document).ready(function(){
+	$("#n9").click(function(event){
+		$("#contentTextBox").load("news_item9.php", function(responseText, statusText, xhr){
+			/*if(statusText==="error"){
+				alert("Error loading content!");
+			}*/
+		});
+	});
+});
+
+$(document).ready(function(){
+	$("#n10").click(function(event){
+		$("#contentTextBox").load("news_item10.php", function(responseText, statusText, xhr){
+			/*if(statusText==="error"){
+				alert("Error loading content!");
+			}*/
+		});
+	});
+});
+/*$.MySpace = {
+	dropLock : 0
+};
+
+
+
+$(document).ready(function(){
+	if( $.cookie("styleSelection") === "1"){
+		$("#styleSelection").val('Inverted');
+		$("#content").css({
+			"background-color": 'black',
+			"color": '#f0f0f0'
+		});
+		$("#indexBar").css({
+			"background-color": '#f0f0f0',
+			"color": 'black',
+			"border-color": 'black'
+		});
+	}
+});
+
+
+
+$(document).ready(function(){
+	$("#toggle").click(function(event){
+		$('#menuUl').toggle();
+		if($('#menu').css("position")==='absolute'){
+			$('#menu').css({"position": 'fixed'});
+		}
+	 	else{
+	 		$('#menu').css({"position": 'absolute'});
+	 	}
+	});
+});
+
+
+
+$(document).ready(function(){	
+	$("a").click(function(event){
+		if($.MySpace.dropLock === 1){
+			event.preventDefault();
+			$(this).parent().hide();
+		}
+	});
+});
+
+
+
+$(document).ready(function(){
+	$("#reset").click(function(event){
+		$("a").parent().show();
+		$("#menuUl").show();
+		});
+	});
+
+	
+$(document).ready(function(){*/
+	/*if( $.cookie("styleSelection") !== "1"){
+		$.cookie("styleSelection", "0");
+	}*/
+	/*if($.cookie("styleSelection") === "1" ){
+		return;
+	}
+	$.cookie("styleSelection", "0");
+});
+
+
+
+$(document).ready(function(){
+	$("#dropLock").click(function(event){
+		if($.MySpace.dropLock === 0){
+			$.MySpace.dropLock = 1;
+			alert("Menu buttons are gonna be dropped on click from now on!");
+		} else {
+			$.MySpace.dropLock = 0;
+			alert("Menu buttons locked.");
+		}
+	});
+});
+
+
+
+$(document).ready(function(){
+	$("#styleSelection").change(function(){
+		if( $("#styleSelection").val() === "Inverted" ){
+			alert("Inverted!");
+			$.cookie("styleSelection", "1");
+		} else 
+		if( $("#styleSelection").val() === "Classic" ){
+			alert("Classic!");
+			$.cookie("styleSelection", "0");
+		}
+		location.reload();
+	});
+});*/
