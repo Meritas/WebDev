5b817ef9ee8fa0e57528ea7901c8b2b893a0cf34
Meritas <bubobuboster@gmail.com>
2013-07-22 11:58:37 +0300

    user registration added

---
+++ newsSite/check.php
@@ -0,0 +1,10 @@
+<?php
+    if(isset($_POST['registration'])){
+        $con = new mysqli("localhost", "root", "", "newssite");
+        $username = strip_tags($_POST['username']);
+        $email = strip_tags($_POST['email']);
+        $fName = strip_tags($_POST['name']);
+        $password = md5(strip_tags($_POST['password']));  
+        $con->query("INSERT INTO users(full_name, email, username, password) VALUES ('$fName', '$email', '$username', '$password')");                                                                                                                                            
+    }
+?>
