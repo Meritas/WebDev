1b4e3887ed74e98eb3296bd3e1d03f1764c2d09c
Meritas <bubobuboster@gmail.com>
2013-08-22 17:44:12 +0300

    new config file

---
+++ newsSite/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$GLOBALS['host'] = "localhost";
+$GLOBALS['dbname'] = "newssite";
+$GLOBALS['user'] = "root";
+$GLOBALS['pass'] = "";
+$GLOBALS['clink'] = new mysqli($GLOBALS['host'], $GLOBALS['user'], $GLOBALS['pass'], $GLOBALS['dbname']);
+
+?>
--- newsSite/phpLib.php
+++ newsSite/phpLib.php
@@ -1,23 +1,20 @@
 <?php
+include_once "config.php";
 	
-	$dbArray = parse_ini_file("./DBconfig.ini");
-	$GLOBALS['clink'] = new mysqli($dbArray['host'], $dbArray['user'], $dbArray['pass'], $dbArray['dbname']);
 	function getCategory( $ord ){
 		$query = $GLOBALS['clink']->query("SELECT name FROM categories WHERE cOrder={$ord};");
 		$fRow = $query->fetch_row();
 		echo $fRow[0];
 	}
 	
-	function getLink( $cID ){
-		$GLOBALS['clink'] = new mysqli("localhost", "root", "", "newssite");
+	function getLink( $cID ){	
 		$query = $GLOBALS['clink']->query("SELECT link FROM categories WHERE cOrder={$cID};");
 		$fRow = $query->fetch_row();
 		echo $fRow[0];
 	}
 	
-	function getTitle( $cID, $ord ){
-		$GLOBALS['clink'] = new mysqli("localhost", "root", "", "newssite");
-		$query = $GLOBALS['clink']->query("SELECT title FROM news_items WHERE cID={$cID} ORDER BY id DESC;");
+	function getTitle( $ord ){
+		$query = $GLOBALS['clink']->query("SELECT title FROM news_items WHERE  cID={$cID} ORDER BY id DESC LIMIT $ord");
 		for( $i=1; $i<=$ord; $i++ ){
 			$fRow = $query->fetch_row();
 			if( $i==$ord ){
@@ -26,8 +23,7 @@
 		}
 	}
 	
-	function getContent( $cID, $ord ){
-		$GLOBALS['clink'] = new mysqli("localhost", "root", "", "newssite");
+	function getContent( $cID, $ord ){		
 		$query = $GLOBALS['clink']->query("SELECT content FROM news_items WHERE cID={$cID} ORDER BY id DESC;");
 		for( $i=1; $i<=$ord; $i++ ){
 			$fRow = $query->fetch_row();
@@ -36,7 +32,6 @@
 	}
 	
 	function getCategoriesExcept( $id ){
-		$GLOBALS['clink'] = new mysqli("localhost", "root", "", "newssite");
 		$query = $GLOBALS['clink']->query("SELECT name, link FROM categories WHERE id!={$id} ORDER BY cOrder ASC;");
 		for( $i=1; $i<=4; $i++ ){
 			$fRow = $query->fetch_row();
@@ -45,7 +40,6 @@
 	}
 	
 	function getNewsInCat( $cID ){
-		$GLOBALS['clink'] = new mysqli("localhost", "root", "", "newssite");
 		$query = $GLOBALS['clink']->query("SELECT title FROM news_items WHERE cID={$cID} ORDER BY id DESC;");
 		for( $i=1; $i<=10; $i++ ){
 			$fRow = $query->fetch_row();
