23c9954cec3a4df0248cd16d959be496d085d3c7
Meritas <bubobuboster@gmail.com>
2013-12-14 15:36:24 +0200

    day9_commit1
    CSS rework(Yoann, Gabby)

--- project/script.js
+++ project/script.js
@@ -38,24 +38,60 @@ function hasExtension(arg){
 	else return 1;
 }
 
-function isPicture( argFile ){
+function isPicture( fileInput, argFile, ev ){
+	var result = new Array(); //[0]=>Success/Failure message; [1]=>Image object on success
+	result[0] = 0;//Failure message by default
+	result[1] = null;//
 	var a = argFile.split(".");
 	format = a[1];
 	if( hasExtension(a)!=1 ){
-		return 0;
+		return result;
 	}
 	else if( (format!="gif")&&(format!="jpeg")&&(format!="jpg")&&(format!="png") ){
-		return 0;
+		return result;
 	}
-	else return 1;
+	else{
+		var f = ev.target.files[0];
+		var fr = new FileReader();
+		var x = resHash[fileInput.id]['x'];
+		var y = resHash[fileInput.id]['y'];
+		fr.onload = function(ev2){
+			console.dir(ev2);
+			var image = new Image();
+			image.src = ev2.target.result;
+			if( (image.width<=0) || (image.height<=0) ){
+				return result;
+			}
+			fr.readAsDataURL(f);				
+			result[0] = 1;
+			result[1] = image;
+		}
+		return result;
+	}
 }
 
 $(document).ready(function(){
 
 	setAllowedRes();
+	(".fileInput").on('change', function(ev){
+		result = isPicture(this, this.value, ev);
+		if( result[0] ){
+			var x = resHash[this.id]['x'];
+			var y = resHash[this.id]['y'];
+			if( result[1].width>x) || (result[1].height>y) ){
+				alert("Invalid image dimensions!");
+				this.value ="";
+			}
+		}else{
+			alert("Invalid file type!")
+			this.value="";
+		}
+	};
 
-	$(".fileInput").on('change', function(ev){
-		if( isPicture(this.value) ){
+});
+
+/*$(".fileInput").on('change', function(ev){
+		if( isPicture(this, this.value, ev) ){
 			var f = ev.target.files[0];
 			var fr = new FileReader();
 			var x = resHash[this.id]['x'];
@@ -66,7 +102,7 @@ $(document).ready(function(){
 				var image = new Image();
 				image.src = ev2.target.result;
 				if( (image.width>x)||(image.height>y) ){
-					alert("Invalide image dimensions!");
+					alert("Invalid image dimensions!");
 					thisFI.value = "";
 				}
 			};
@@ -76,6 +112,5 @@ $(document).ready(function(){
 			alert("Invalide file type!");
 			this.value = "";
 		}
-	});
+	});*/
 
-});
